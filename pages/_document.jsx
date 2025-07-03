d3dfa65f697088611badfb25047c1ddeecbe9527
nickdelicto <nick.crystalconnect@gmail.com>
2025-07-03 00:59:37 -0400

    Add Google Analytics Tag

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,6 +4,22 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+            `,
+          }}
+        />
+        
         {/* Character encoding */}
         <meta charSet="utf-8" />
         
