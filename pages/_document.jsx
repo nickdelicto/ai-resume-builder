6926be16828b2cb36d63a35cb593ee4bc547240f
nickdelicto <nick.crystalconnect@gmail.com>
2025-11-22 01:26:30 -0500

    Add Microsoft Clarity analytics for user behavior tracking

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -20,6 +20,19 @@ export default function Document() {
           }}
         />
         
+        {/* Microsoft Clarity - Heatmaps & Session Recordings */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "u9wvv8gym2");
+            `,
+          }}
+        />
+        
         {/* Character encoding */}
         <meta charSet="utf-8" />
         
